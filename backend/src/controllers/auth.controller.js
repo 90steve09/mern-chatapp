7e6645e2cc125d0e2a09e929486a9e6448e0d5f7
steve <sdelima09@gmail.com>
2025-04-15 11:19:10 +0800

    frontend changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,45 @@
-export const signup = (req, res) =>{
-    res.send("signup route")
+import bcrypt from "bcrypt"
+import userModel from "../models/user.model.js"
+import { generateToken } from "../lib/utils.js"
+
+
+
+export const signup = async (req, res) =>{
+    const {fullname, email, password} = req.body
+
+    try {
+        //has password
+         if(password.length > 6){
+            return  res.status(400).json({success: false , message: "Password must be at least 6 characrter"})
+         }
+
+         const user = userModel.findOne({email})
+
+         if(user) return res.status(400).json({success:false , message : "Email already exist"})
+
+         const salt = await bcrypt.genSalt(10);
+         const hashPassword = await bcrypt.hash(password, salt)
+         
+        const newUser =  new userModel({fullName, email, password: hashPassword})
+        
+
+        if(newUser){
+            //generate jwt
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+
+        }else{
+            return res.status(400).json({message: false , message: "Invalid user data"})
+        }
+
+
+
+        
+    } catch (error) {
+        return res.status(500).json({success:false , message: error})
+    }
+
 }
 export const login = (req, res) =>{
     res.send("login route")
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { json } from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
@@ -7,6 +7,8 @@ dotenv.config()
 
 
 const app = express();
+app.use(express.json())
+
 app.use("/api/auth", authRoutes)
 
 
